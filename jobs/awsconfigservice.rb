e502826f292c1fc5667c8623b32e5bb29d48e286
JPD <JermilaPaulDhas@users.noreply.github.com>
2017-09-19 18:02:06 +0100

    Re-evaluate config rules to remove deleted resources

--- jobs/awsconfigservice.rb
+++ jobs/awsconfigservice.rb
@@ -18,9 +18,20 @@ SCHEDULER.every '1m', :first_in => 0 do
 
         resp = awsconfigservice.describe_config_rules()
 
-
         resp.config_rules.each do |rules|
             config_rule = rules.config_rule_name
+            
+            # Clear the evalutaion results
+            awsconfigservice.delete_evaluation_results({
+                    config_rule_name: config_rule,
+            })
+            
+            # Start the evaluation of the config rule
+            awsconfigservice.start_config_rules_evaluation({
+                    config_rule_name: config_rule,
+            })
+            
+            # Get compliance results
             resources = awsconfigservice.get_compliance_details_by_config_rule({
                 config_rule_name: config_rule,
                 compliance_types: ["NON_COMPLIANT"],
